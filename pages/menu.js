7bf75713586da81e3bc0acdf2fe2025f865d792b
Sreebash Chandra Das <sreebashdas.1@gmail.com>
2021-12-01 11:43:35 +0600

    Imperative Routing

--- pages/menu.js
+++ pages/menu.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Link from "next/link";
+import Router from "next/router";
 
 class Menu extends Component {
     render() {
@@ -9,6 +10,7 @@ class Menu extends Component {
                 <ul>
                     <li><Link href='/'>Home</Link></li>
                     <li><Link href={{ pathname: '/about', query: {firstName: 'Sreebash Kumar', lastName: 'Das'}}}>About</Link></li>
+                    <li><button onClick={()=>Router.push('/about')}>About</button></li>
                     <li><Link href='/contact'>Contact</Link></li>
                 </ul>
             </div>
