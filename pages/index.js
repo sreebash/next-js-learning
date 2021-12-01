9153d417a4a6c589e7fff501b7e9498b005efc74
Sreebash Chandra Das <sreebashdas.1@gmail.com>
2021-12-01 11:22:38 +0600

    environment variable

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
 import Menu from "./menu";
 
+
+export async function getStaticProps() {
+    return {
+        props: {
+            APIKEY: process.env.APIKEY
+        }
+    }
+}
+
 class Index extends Component {
     render() {
         return (
             <div>
                 <Menu/>
                 <h1>Index Page</h1>
+                <h1>API KEY: {this.props.APIKEY}</h1>
             </div>
         );
     }
